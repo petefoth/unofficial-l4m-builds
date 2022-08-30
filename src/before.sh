fc6ec655a52b38f5354a7fb2442619b1178746de
Pete Fotheringham <petefoth@e.email>
2022-08-30 10:00:00 +0100

    Update before.sh
    Fix #20 Sort out zip file naming
    Revise the comment formatting

--- src/before.sh
+++ src/before.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 #########################################################
-# Get and apply the restricted signature spoofing patches
+# - Get and apply the restricted signature spoofing patches
+# - Sort out zip file naming
 #
-# we get the patches from the L4M docker-lineage-cicd repo
-# and the logic from their Dockerfile
 #
 # this file should be copied to the userscripts directory
 # .e.userscripts, so it will be called by android_vendor_e/post-sync.sh
 #########################################################
+
+#########################################################
+# Get and apply the restricted signature spoofing patches
+#    we get the patches from the L4M docker-lineage-cicd repo
+#    and the logic from their Dockerfile
 
 rm -rf ${TMP_DIR}/l4m-cicd
 git clone https://github.com/lineageos4microg/docker-lineage-cicd.git ${TMP_DIR}/l4m-cicd
@@ -37,3 +41,18 @@ sed 's/android:protectionLevel="dangerous"/android:protectionLevel="signature|pr
 
 git clean -q -f
 cd ../..
+#########################################################
+
+#########################################################
+# Sort out zip file naming
+
+makefile_containing_version="vendor/$vendor/config/common.mk"
+
+if [ -f "vendor/$vendor/config/version.mk" ]; then
+  makefile_containing_version="vendor/$vendor/config/version.mk"
+fi
+
+echo ">> [$(date)] Setting \"$RELEASE_TYPE\" as release type"
+sed -i "/\$(filter .*\$(${vendor^^}_BUILDTYPE)/,/endif/d" "$makefile_containing_version"
+
+#########################################################
